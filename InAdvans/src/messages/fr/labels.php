6698a491ec1bb7f0685e65a5a8f158477e2ff7bb
prerot <pierre.rerot@inadvans.com>
2019-01-30 11:23:51 +0100

    ok

---
+++ InAdvans/src/messages/fr/labels.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ *
+ */
+return [
+    "Audit Trail" => "Audit trail",
+    "Audit Trails" => "Audit trails",
+    "Context Type" => "Contexte",
+    "Data" => "Données",
+    "Event Type" => "Evénement",
+    "Message" => "Message",
+    "Transaction ID" => "Transaction",
+    "User ID" => "id. utilisateur",
+    "User Information" => "Utilisateur",
+];
